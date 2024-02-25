e18c191ce517527d0a88525e00cb3dadd8450350
Baden Ashford <baden.ashford@gmail.com>
2024-02-25 20:18:16 +1100

    Fiddle

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,20 +3,27 @@ class CalendarsController < ApplicationController
     end
 
     def create
+        if form_params[:pbl] == "" || form_params[:clin] == ""
+            flash[:alert] = "You must choose both a pbl and a clin"
+            render :new and return
+        end
+
         response = Aws::LambdaClient.call("jmp_calendar", {
-            pbl: "K",
-            clin: "18",
+            pbl: form_params[:pbl],
+            clin: form_params[:clin],
             key: "MEDI1101A Timetable Weeks 1 to 3 2024 - Callaghan & Central Coast.xlsx"
         })
 
         calendar = Icalendar::Calendar.parse(response.payload).first.to_ical
 
-        send_data calendar, type: 'text/calendar', filename: 'event.ics'
+        calendar_name = "pbl_#{form_params[:pbl]}_clin_#{form_params[:clin]}_#{Time.zone.now.strftime("%Y%m%d%H%M%S")}.ics"
+
+        send_data calendar, type: 'text/calendar', filename: calendar_name
     end
 
     private
 
     def form_params
-        params.require(:form).permit(:clin, :pbl)
+        params.permit(:clin, :pbl)
     end
 end
