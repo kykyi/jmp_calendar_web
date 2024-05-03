f25714a7ae3af4ee45fe8958580a10d4bf660746
Baden Ashford <baden.ashford@gmail.com>
2024-05-03 11:47:18 +1000

    Create calendars table

---
+++ db/migrate/20240503014406_create_calendars.rb
@@ -0,0 +1,9 @@
+class CreateCalendars < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :calendars do |t|
+      t.string :raw, null: false
+      t.timestamps
+    end
+  end
+end
