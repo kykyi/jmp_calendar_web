ba5d49f16428381cc03b9d7699af625b0396dc46
Baden Ashford <baden.ashford@gmail.com>
2024-05-03 12:06:24 +1000

    Update to pg, include migration file

---
+++ db/migrate/20240503015309_add_cols_to_calendar.rb
@@ -0,0 +1,9 @@
+class AddColsToCalendar < ActiveRecord::Migration[7.0]
+  def change
+    add_column :calendars, :pbl, :string
+    add_column :calendars, :clin, :string
+    add_column :calendars, :year, :string
+    add_column :calendars, :uni, :string
+    add_column :calendars, :spreadsheet, :string
+  end
+end
