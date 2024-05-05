7983b1037b7bf06f059e293d82d8e362159dbf78
Baden Ashford <baden.ashford@gmail.com>
2024-05-05 11:37:57 +1000

    Make SENTRY_DSN an env var

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 
 
 Sentry.init do |config|
-  config.dsn = 'https://85bfa2d8f5e821b2dc9667d407fcee3d@o4506799250407424.ingest.sentry.io/4506806914449408'
+  config.dsn = ENV.fetch('SENTRY_DSN', "")
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   # Disable performance monitoring
   config.traces_sample_rate = 0.1
