a00bd3cc0aa19533cf22c9d8738ad6fe39cebf53
Baden Ashford <baden.ashford@gmail.com>
2024-08-08 19:58:05 +1000

    Bump UON year 2

--- spec/system/spreadsheets_are_valid_spec.rb
+++ spec/system/spreadsheets_are_valid_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'Spreadsheets contain no errors' do
   describe 'UON Year 2' do
     let(:uni) { 'UON' }
     let(:year) { 2 }
-    let(:spreadsheet) { load_spreadsheet(uni, year, '2024MEDI2101BTimetable-CANVAS.xlsx') }
+    let(:spreadsheet) { load_spreadsheet(uni, year, '2024 MEDI2101B Timetable - 30 Aug.xlsx') }
 
     ('A'..'Q').to_a.each do |pbl|
       it_behaves_like 'a valid combination' do
